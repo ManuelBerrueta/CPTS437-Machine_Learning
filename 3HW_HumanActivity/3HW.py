c7eb9cd0891e40a13183cae87461ebf257afbc9b
Berrueta, Manuel <manuel.berrueta@gmail.com>
2020-02-18 07:24:45 -0800

    Start of HW3

---
+++ 3HW_HumanActivity/3HW.py
@@ -0,0 +1,21 @@
+'''First, write code to calculate accuracy and a macro f-measure using 3-fold
+ cross validation for two classifiers: a majority classifier and a decision tree
+  classifier. You can use the sklearn libraries for the classifiers but write
+   your own code to perform cross validation and calculation of the
+    performance measures.'''
+
+'''Second, provide your observations on the results. Why do the two performance
+ measures provide such different results? Why do the two classifiers perform
+  so differently on this task?'''
+
+
+'''Third, write code to generate and plot an ROC curve
+ (containing at least 10 points). Generate two ROC curves, one based on a
+  decision tree classifier with a depth bound of 2 and one with an unbounded
+   decision tree. You can use the sklearn predict_proba function to provide a
+    probability distribution over the class values, but create your own
+     ROC curve rather than using the sklearn roc_curve function. Ideally you
+      would generate the ROC curve on a holdout subset of data, but for
+       simplicity in this case you can build it using the entire dataset.'''
+
+
