74209880fbcf6e5220f19beed39fd32c186c0998
Berrueta, Manuel <manuel.berrueta@gmail.com>
2020-02-24 22:31:13 -0800

    Updated Code, looking a lot better; Need to figure out cross validation code and ROC Curve

--- 3HW_HumanActivity/3HW.py
+++ 3HW_HumanActivity/3HW.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
 
 # Majority Classifier
 from sklearn.dummy import DummyClassifier
@@ -36,37 +37,69 @@ print("Split Data\n")
 X_trainfeatures, X_testfeatures, y_traininglabels, y_testlabels = train_test_split(X_features, y_label, test_size = .3)#, random_state = 7919)
 
 print("Target Labels Test:", y_traininglabels)
+
 '''First, write code to calculate accuracy and a macro f-measure using 3-fold
  cross validation for two classifiers: a majority classifier and a decision tree
   classifier. You can use the sklearn libraries for the classifiers but write
    your own code to perform cross validation and calculation of the
     performance measures.'''
 
-clfMajority = DummyClassifier()
-clfMajority.fit(X_trainfeatures, y_traininglabels)
 
-#TODO: Code to Calculate Accuracy
+
+def accuracy(TP, FP, TN, FN):
+    P = TP + FN
+    N = TN + FP
+    return ((TP + TN) / (P + N))
+
+def precision(TP, FP):
+    return (TP / (TP + FP))
+
+def recall(TP, FN):
+    return (TP / (TP + FN))
+
+
+def F_Measure(TP, FP, TN, FN):
+    P = precision(TP, FP)
+    R = recall(TP, FN)
+    F = ((2 * P * R) / (P + R))
+    return F
 
 #TODO: Code to macro f-measure using 3-fold cross validation
+def cross_validate(algo, data, K):
+    pass
 
 
-boundedTree = DecisionTreeClassifier(criterion="entropy", max_depth=2)
-boundedTree.fit(X_trainfeatures, y_traininglabels)
-boundScore = boundedTree.predict_proba(X_testfeatures)
-boundScore = np.array(boundScore)
-print("Bounbded Score:")
-print(boundScore)
+clfMajority = DummyClassifier()
+clfMajority.fit(X_trainfeatures, y_traininglabels)
+clfMajorityPrediction = clfMajority.predict(X_testfeatures)
+tn, fp, fn, tp = confusion_matrix(y_testlabels, clfMajorityPrediction).ravel()
+clfMajority_conf_matrix = confusion_matrix(y_testlabels, clfMajorityPrediction)
+print("Accuracy of Majority Classifier: " + str(accuracy(tp,fp,tn,fn)))
+print("F-Measure of Majority Classifier: " + str(F_Measure(tp,fp,tn,fn)))
 
-unboundedTree = DecisionTreeClassifier(criterion="entropy") #Unbounded
-unboundedTree.fit(X_trainfeatures, y_traininglabels)
-unboundedScore = unboundedTree.predict_proba(X_testfeatures)
-unboundedScore = np.array(unboundedScore)
-print("Unbounbded Score:")
-print(unboundedScore)
+
+clfTree = DecisionTreeClassifier(criterion="entropy") #Unbounded
+clfTree.fit(X_trainfeatures, y_traininglabels)
+clfTreePrediction = clfTree.predict(X_testfeatures)
+tn, fp, fn, tp = confusion_matrix(y_testlabels, clfTreePrediction).ravel()
+clfTree_conf_matrix = confusion_matrix(y_testlabels, clfTreePrediction)
+print("Accuracy of Decision Tree Classifier: " + str(accuracy(tp,fp,tn,fn)))
+print("F-Measure of Decision Tree Classifier: " + str(F_Measure(tp,fp,tn,fn)))
 
 '''Second, provide your observations on the results. Why do the two performance
  measures provide such different results? Why do the two classifiers perform
   so differently on this task?'''
+
+''' They provide such different results because of their biases and they way 
+they categorize the data. They perform so different because the Majority 
+Classifier is really an either or classifier where literally the majority wins,
+ where as the Tree will branch out to look at the possibilities of being in one
+  class or another. '''
+
+
+
+
+
 
 
 '''Third, write code to generate and plot an ROC curve
@@ -78,6 +111,36 @@ print(unboundedScore)
       would generate the ROC curve on a holdout subset of data, but for
        simplicity in this case you can build it using the entire dataset.'''
 
+boundedTree = DecisionTreeClassifier(criterion="entropy", max_depth=2)
+boundedTree.fit(X_trainfeatures, y_traininglabels)
+boundedPrediction = boundedTree.predict(X_testfeatures)
+tn, fp, fn, tp = confusion_matrix(y_testlabels, boundedPrediction).ravel()
+bounded_conf_matrix = confusion_matrix(y_testlabels, boundedPrediction)
+print("Confusion Matrix")
+print(tn, fp, fn, tp)
+print(bounded_conf_matrix)
+""" boundScore = boundedTree.predict_proba(X_testfeatures)
+boundScore = np.array(boundScore)
+print("Bounbded Score:")
+print(boundScore)
+ """
+
+
+
+unboundedTree = DecisionTreeClassifier(criterion="entropy") #Unbounded
+unboundedTree.fit(X_trainfeatures, y_traininglabels)
+unboundedPrediction = unboundedTree.predict(X_testfeatures)
+tn, fp, fn, tp = confusion_matrix(y_testlabels, unboundedPrediction).ravel()
+unbounded_conf_matrix = confusion_matrix(y_testlabels, unboundedPrediction)
+print("Confusion Matrix")
+print(tn, fp, fn, tp)
+print(unbounded_conf_matrix)
+""" unboundedScore = unboundedTree.predict_proba(X_testfeatures)
+unboundedScore = np.array(unboundedScore)
+print("Unbounbded Score:")
+print(unboundedScore)
+ """
+
 
 # SKlearn roc curve for testing
 
@@ -88,10 +151,11 @@ print(unboundedScore)
 
 """ plt.title('Receiver Operating Characteristic')
 plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
-plt.legend(loc = 'lower right')
 plt.plot([0, 1], [0, 1],'r--')
 plt.xlim([0, 1])
 plt.ylim([0, 1])
+plt.legend(loc = 'lower right')
 plt.ylabel('True Positive Rate')
 plt.xlabel('False Positive Rate')
+plt.tile("ROC Curve)
 plt.show() """
